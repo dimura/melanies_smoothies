c07f805538296e28ed706699a2a6890f6837e423
dimura <dimura@web.de>
2026-02-20 01:12:50 +0100

    Update streamlit_app.py
    error1

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ name_on_order = st.text_input('Name on smoothie:')
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 ingredients_list = st.multiselect('Choose up to 5', my_dataframe,  max_selections=5)
 
 if ingredients_list:
