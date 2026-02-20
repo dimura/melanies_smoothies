a8458367865cd1c35e16885df5012ead1a0c246d
dimura <dimura@web.de>
2026-02-20 01:10:31 +0100

    Update streamlit_app.py
    search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ if ingredients_list:
     ingredients_string = ''
     for i in ingredients_list:
         ingredients_string+=i + ' '
-        st.subheader(i + 'Nutrition Information')
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{i}")
+        st.subheader(i + ' Nutrition Information')
+        search_val = my_dataframe[my_dataframe['FRUIT_NAME']==]['SEARCH_ON'].values[0]
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_val}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
